f23532104f1850c479e74cefb23502f0b84921a4
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-21 16:35:03 -0700

    feat(events-v2): Move fetching event data to main events component

--- src/sentry/static/sentry/app/views/organizationEventsV2/events.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/events.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
 import SentryTypes from 'app/sentryTypes';
+import AsyncComponent from 'app/components/asyncComponent';
+import {getQuery} from './utils';
 
-export default class Events extends React.Component {
+export default class Events extends AsyncComponent {
   static propTypes = {
+    organization: SentryTypes.Organization.isRequired,
     view: SentryTypes.EventView.isRequired,
   };
-  render() {
+
+  getEndpoints() {
+    const {organization, view} = this.props;
+    return [
+      [
+        'events',
+        `/organizations/${organization.slug}/events/`,
+        {
+          query: getQuery(view),
+        },
+      ],
+    ];
+  }
+
+  renderBody() {
     return this.props.view.name;
   }
 }
